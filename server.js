f8f53d16a518400abd33cf83a53662ccba20cd4d
sergios1 <sergetnazaretyan@gmail.com>
2021-01-09 18:12:10 -0800

    a

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.get("/", function (req, res) {
 });
 
 
+app.get("/api/timestamp/",(req,res) =>{
+  res.json({hi: "bis"});
+})
+
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
